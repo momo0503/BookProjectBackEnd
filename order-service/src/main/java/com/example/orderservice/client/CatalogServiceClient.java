652fabb41f5b9820db450fc12a14b1afd41c08d8
haribowoo <85086591+hariBoWoo@users.noreply.github.com>
2021-09-15 22:06:00 +0900

    jinhui

--- order-service/src/main/java/com/example/orderservice/client/CatalogServiceClient.java
+++ order-service/src/main/java/com/example/orderservice/client/CatalogServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="catalog-service", url="52.20.91.194:8000")
+@FeignClient(name="catalog-service")
 public interface CatalogServiceClient {
 
     @GetMapping("/catalogs/{productId}")
